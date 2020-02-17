3a4b27ef0f78f7a2b673008de34025eb37036a9e
Jack Willis <jack@attac.us>
2020-02-16 22:00:30 -0600

    Simplify militanthistory.js

--- scripts/militanthistory.js
+++ scripts/militanthistory.js
@@ -6,23 +6,18 @@ WebFont.load({ custom: { families: ['Alegreya Sans SC'] } });
 // custom behavior
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Allow users to click anywhere inside a `.breadcrumb li`, not just within the `a`.
-  var breadcrumbElements = document.querySelectorAll('.breadcrumb li');
-  for (var i = 0; i < breadcrumbElements.length; i++) {
-    var element = breadcrumbElements[i];
-    var link = element.getElementsByTagName('a')[0];
-
-    (function(link) {
-      element.addEventListener('click', function() {
-        link.click();
-      });
-    })(link);
+  // Allow users to click anywhere inside a `.breadcrumb li` / "crumb", not just within the `a`.
+  var crumbs = document.querySelectorAll('.breadcrumb li');
+  for (var i = 0; i < crumbs.length; i++) {
+    var crumb = crumbs[i];
+    var link = crumb.getElementsByTagName('a')[0];
+    crumb.addEventListener('click', link.click.bind(link));
   }
 
   // JS-only styles
-  var breadCrumbStyle = '.breadcrumb li:hover { cursor: pointer; }';
+  var breadcrumbRule = '.breadcrumb li:hover { cursor: pointer; }';
 
   var style = document.createElement('style');
-  style.innerText = breadCrumbStyle;
+  style.innerText = breadcrumbRule;
   document.head.appendChild(style);
 });
