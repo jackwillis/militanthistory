52cf53063c14931b9c07354c471d2d3bda62a236
Jack Willis <jack@attac.us>
2020-02-24 02:36:40 -0600

    Use text-balancer and font-face-observer

--- scripts/militanthistory.js
+++ scripts/militanthistory.js
@@ -1,2 +1,21 @@
 // militanthistory.js
 
+document.addEventListener('DOMContentLoaded', function() {
+  var bodyTextFont = new FontFaceObserver('Alegreya Sans');
+  var smallCapsFont = new FontFaceObserver('Alegreya Sans SC');
+  var displayFont = new FontFaceObserver('Titillium Web');
+
+  smallCapsFont.load().then(function() {
+    document.body.classList.add('small-caps-font-loaded');
+  });
+
+  var allFonts = Promise.race([
+    bodyTextFont.load(),
+    smallCapsFont.load(),
+    displayFont.load()
+  ]);
+
+  allFonts.then(function() {
+    textBalancer.initialize('h2, h3, h4, .lead, .subtitle, td');
+  });
+});
